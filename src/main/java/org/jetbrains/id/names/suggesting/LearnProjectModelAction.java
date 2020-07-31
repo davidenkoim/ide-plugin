e327f3e542270c5233357b3a9e8d2c1b06769732
Igor.Davidenko <igor.davidenko@jetbrains.com>
2020-08-01 00:44:03 +0300

    action fix

--- src/main/java/org/jetbrains/id/names/suggesting/LearnProjectModelAction.java
+++ src/main/java/org/jetbrains/id/names/suggesting/LearnProjectModelAction.java
@@ -1,17 +1,14 @@
 package org.jetbrains.id.names.suggesting;
 
-import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -22,10 +19,7 @@ public class LearnProjectModelAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = Objects.requireNonNull(e.getProject());
         ModelService service = ModelService.getInstance(project);
-        FileEditor fileEditor = (FileEditor) e.getDataContext().getData("FILE_EDITOR");
-        assert fileEditor != null: "fileEditor is null";
-        VirtualFile virtualFile = Objects.requireNonNull(fileEditor.getFile());
-        PsiFile file = Objects.requireNonNull(PsiManager.getInstance(project).findFile(virtualFile));
+        PsiFile file = Objects.requireNonNull(e.getData(LangDataKeys.PSI_FILE));
         ProgressManager.getInstance().run(new Task.Backgroundable(project, "Building project id suggesting models") {
             @Override
             public void run(@NotNull ProgressIndicator progressIndicator) {
