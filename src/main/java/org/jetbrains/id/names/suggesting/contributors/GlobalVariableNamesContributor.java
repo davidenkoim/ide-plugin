efd685f4d9148acad45201907b224e88148b66bf
Igor.Davidenko <igor.davidenko@jetbrains.com>
2020-08-07 15:53:34 +0300

    add GlobalVariableNamesContributor and action to train it

---
+++ src/main/java/org/jetbrains/id/names/suggesting/contributors/GlobalVariableNamesContributor.java
@@ -0,0 +1,21 @@
+package org.jetbrains.id.names.suggesting.contributors;
+
+import com.intellij.psi.PsiVariable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.id.names.suggesting.IdNamesSuggestingModelManager;
+import org.jetbrains.id.names.suggesting.api.IdNamesSuggestingModelRunner;
+import org.jetbrains.id.names.suggesting.api.VariableNamesContributor;
+
+import java.util.LinkedHashSet;
+
+public class GlobalVariableNamesContributor implements VariableNamesContributor {
+    @Override
+    public void contribute(@NotNull PsiVariable variable, @NotNull LinkedHashSet<String> resultSet) {
+        IdNamesSuggestingModelRunner modelRunner = IdNamesSuggestingModelManager.getInstance(variable.getProject())
+                                                                                .getModelRunner(this.getClass().getName());
+        if (modelRunner == null) {
+            return;
+        }
+        resultSet.addAll(modelRunner.suggestNames(variable));
+    }
+}
---
+++ src/main/java/org/jetbrains/id/names/suggesting/impl/TrainGlobalNGramModelAction.java
@@ -0,0 +1,42 @@
+package org.jetbrains.id.names.suggesting.impl;
+
+import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.id.names.suggesting.IdNamesSuggestingBundle;
+import org.jetbrains.id.names.suggesting.IdNamesSuggestingModelManager;
+import org.jetbrains.id.names.suggesting.api.AbstractTrainModelAction;
+
+public class TrainGlobalNGramModelAction extends AbstractTrainModelAction {
+    @Override
+    protected void doActionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        assert project != null;
+        IdNamesSuggestingModelManager modelManager = IdNamesSuggestingModelManager.getInstance(project);
+        ProgressManager.getInstance().run(new Task.Backgroundable(project, IdNamesSuggestingBundle.message("training.task.title")) {
+            @Override
+            public void run(@NotNull ProgressIndicator progressIndicator) {
+                progressIndicator.setText(IdNamesSuggestingBundle.message("training.progress.indicator.text", project.getName()));
+                ReadAction.nonBlocking(() -> {
+                    modelManager.trainGlobalNGramModel(project, progressIndicator);
+                })
+                        .inSmartMode(project)
+                        .executeSynchronously();
+            }
+        });
+    }
+
+    @Override
+    protected boolean canBePerformed(@NotNull AnActionEvent e) {
+        return e.getProject() != null &&
+                FileTypeIndex.containsFileOfType(JavaFileType.INSTANCE, GlobalSearchScope.projectScope(e.getProject()));
+    }
+}
