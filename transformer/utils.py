41fda4af60ac1e10cd917cfb6b7b62165bf188b0
Igor Davidenko <60134138+davidenkoim@users.noreply.github.com>
2021-03-05 13:08:01 +0300

    add special tokens and sub_token_split

--- transformer/utils.py
+++ transformer/utils.py
@@ -3,6 +3,18 @@ import re
 
 import torch
 
+INIT_TOKEN = "<s>"
+EOS_TOKEN = "</s>"
+PAD_TOKEN = "<pad>"
+UNK_TOKEN = "<unk>"
+VAR_TOKEN = "<var>"
+TOKEN_DELIMITER = "\u2581"
+
+JAVA_SMALL_TRAIN = {"cassandra-dtest-shaded", "gradle", "intellij-community", "presto-root", "wildfly-parent",
+                    "elasticsearch", "hibernate-orm", "liferay-portal", "spring"}
+JAVA_SMALL_VAL = {"gdx-parent"}
+JAVA_SMALL_TEST = {"hadoop"}
+
 
 def generate_padding_mask(num_usages, max_usages=None, device=None):  # B
     """
@@ -61,36 +73,69 @@ def read_json(file):
         return json.loads(f.read())
 
 
-def camelCaseSplit(name):
+def to_list(name):
+    return [name]
+
+
+def split(name):
+    return name.split(TOKEN_DELIMITER)
+
+
+def camel_case_split(name):
     r"""
     Splits CamelCase name to tokens and removes underscore character.
 
     Example:
 
-    >>> camelCaseSplit('CamelCase_word') == ['camel', 'case', 'word']
+    >>> camel_case_split('__CamelCase_word__') == ['__', 'camel', 'case', 'word', '__']
 
     :param name: name that you want to tokenize,
     :return: list of tokens.
     """
-    return re.sub('([A-Z][a-z]+)', r' \1',
+    if name == VAR_TOKEN:
+        return [name]
+    return re.sub('([A-Z]?[a-z]+)', r' \1 ',
                   re.sub('([A-Z]+)', r' \1',
-                         re.sub('(_+)', r' ', name)
+                         re.sub(r'(?<=[A-Za-z0-9])(_+)(?=[A-Za-z0-9])', r' ', name)
                          )
                   ).lower().split()
 
 
-def camelCaseTokenizer(b=True):
+def sub_token_split(name, length=None):
+    # length must be odd
+    tokens = name.split(TOKEN_DELIMITER)
+    subtokens = []
+    if length is None:
+        length = len(tokens)
+    center = len(tokens) // 2
+    l = length // 2
+    left_side, right_side = [], []
+    for i in range(center - 1, 0, -1):
+        left_side.extend(reversed(camel_case_split(tokens[i])))
+        if len(left_side) >= l:
+            break
+    left_side = list(reversed(left_side[:l]))
+    for i in range(center + 1, len(tokens)):
+        right_side.extend(camel_case_split(tokens[i]))
+        if len(right_side) >= l:
+            break
+    right_side = right_side[:l]
+    return left_side + [tokens[center]] + right_side
+
+
+def sub_tokenizer(length=11):
+    return lambda x: sub_token_split(x, length)
+
+
+def camel_case_tokenizer(b=True):
     """
     :param b: True -- return function that tokenize name as CamelCase string, False -- string -> [string],
     :return: tokenization function.
     """
     if b:
-        return camelCaseSplit
+        return camel_case_split
     else:
-        def toList(name):
-            return [name]
-
-        return toList
+        return to_list
 
 
 def reverseSplit(string):
