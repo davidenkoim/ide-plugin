f346f1223cb9233777b6a569346092025e08ebf8
Igor Davidenko <60134138+davidenkoim@users.noreply.github.com>
2021-03-05 13:08:57 +0300

    train file change

--- transformer/trainIdTransformerModel.py
+++ transformer/trainIdTransformerModel.py
@@ -1,40 +1,50 @@
+import logging
+import warnings
+
 import hydra
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
-from idTransformerModel import IdTransformerModel
-from idDataModule import IdDataModule
-import logging
+from dataset.idDataModule import IdDataModule
+from model.idTransformerModel import IdTransformerModel
+
+logger = logging.getLogger("wandb")
+logger.setLevel(logging.WARNING)
+warnings.filterwarnings("ignore")
 
 
 @hydra.main(config_path='configs', config_name='config')
 def train(cfg):
-    datamodule = IdDataModule(cfg.dataset_path, cfg.dataset_split_strategy, cfg.model)
-    datamodule.setup('fit')
-    model = IdTransformerModel(datamodule, cfg.model)
     name_of_run = cfg.model.name_of_run
-
     logger = True
     if cfg.show_in_wandb:
-        # Turn off wandb warnings.
-        logger = logging.getLogger("wandb")
-        logger.setLevel(logging.WARNING)
         # Initializing wandb logger.
         logger = WandbLogger(name=name_of_run, project='IdTransformer', log_model=True)
-        logger.log_hyperparams(cfg.model)
-        logger.watch(model, log='all', log_freq=50)
+        logger.log_hyperparams(cfg)
+
+    datamodule = IdDataModule(cfg)
+    datamodule.setup('fit')
+    model = IdTransformerModel(datamodule, cfg.model)
+
+    if cfg.show_in_wandb:
+        logger.watch(model, log='all')
+    monitor_metric = "val_top1" if model.tusk == "classification" else "val_accuracy"
     checkpoint_callback = ModelCheckpoint(save_last=True,
-                                          save_top_k=5,
-                                          monitor='val_loss',
-                                          dirpath=cfg.checkpoints_dir,
-                                          filename=name_of_run + '-{epoch:02d}-{val_mrr:.2f}')
+                                          save_top_k=3,
+                                          mode="max",
+                                          monitor=monitor_metric,
+                                          dirpath=cfg.model.checkpoints_dir,
+                                          filename=f"{name_of_run}-{cfg.dataset.name}-{{epoch:02d}}-{{{monitor_metric}:.2f}}")
     trainer = pl.Trainer(max_epochs=cfg.model.max_epochs,
                          logger=logger,
                          gpus=cfg.model.gpus,
-                         weights_summary='full',
-                         callbacks=[checkpoint_callback])
-    trainer.fit(model, datamodule)
+                         weights_summary='top',
+                         callbacks=[checkpoint_callback],
+                         log_every_n_steps=10,
+                         val_check_interval=0.5,
+                         resume_from_checkpoint=None)
+    trainer.fit(model, datamodule=datamodule)
 
 
 if __name__ == "__main__":
