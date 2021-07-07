5cde514cb80d7ae0367c548f82adffc98d766dad
Igor Davidenko <60134138+davidenkoim@users.noreply.github.com>
2021-07-07 19:34:00 +0300

    small change in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -148,15 +148,15 @@ tasks {
 //    }
 
 //    (graph)ModelsEvaluator part
-    runIde {
-        val evaluatorToUse: String? by project
-        val dataset: String? by project
-        val saveDir: String? by project
-        val ngramContributorType: String? by project
-        args = listOfNotNull(evaluatorToUse, dataset, saveDir, ngramContributorType)
-        jvmArgs = listOf("-Djava.awt.headless=true")
-        maxHeapSize = "8g"
-    }
+//    runIde {
+//        val evaluatorToUse: String? by project
+//        val dataset: String? by project
+//        val saveDir: String? by project
+//        val ngramContributorType: String? by project
+//        args = listOfNotNull(evaluatorToUse, dataset, saveDir, ngramContributorType)
+//        jvmArgs = listOf("-Djava.awt.headless=true")
+//        maxHeapSize = "8g"
+//    }
 
 
     register("generateUpdatePluginsXML") {
